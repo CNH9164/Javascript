abbd498dfe42f0442685a8e33d60d7459aef5edf
CNH9164 <vishnuvardhanvvr9164@gmail.com>
2026-02-24 08:01:07 +0530

    Var,const,let

--- JSBASICS/1.js
+++ JSBASICS/1.js
@@ -3,4 +3,11 @@ let accountEmail="pranessh@gmail.com"
 var accountPassword="9164"
 accountName="cnh"
 
+//The data in const keyword cannot be changed
+
 console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountName]); //This is used to represent the data in tabluar format
+
+/*
+    prefer not to use var keyword this is beacuse of issues in block scope and functional scope
+*/
